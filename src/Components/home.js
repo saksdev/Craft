03503e422c77da6b217685893f8119be95772074
Saksham Sharma <70628680+saksdev@users.noreply.github.com>
2024-04-16 22:30:31 +0530

    Update home.js

--- src/Components/home.js
+++ src/Components/home.js
@@ -19,24 +19,6 @@ function Home() {
 			<div className='booktable'>
 				<h4>Book a Table &rarr;</h4>
 				<div className='tabledate'>
-					{/* <label htmlFor='date'>Date
-								<svg viewBox="0 0 24 24">
-									<path d="M21.71,8.21l-9,9a1,1,0,0,1-1.42,0l-9-9A1,1,0,1,1,3.71,6.79L12,15.09l8.29-8.3a1,1,0,1,1,1.42,1.42Z" data-name="20 Down Arrow"></path>
-								</svg>
-							</label>
-						<input id='date' type='date' placeholder='2001-12-01' />
-						<label>Date
-							<svg viewBox="0 0 24 24">
-								<path d="M21.71,8.21l-9,9a1,1,0,0,1-1.42,0l-9-9A1,1,0,1,1,3.71,6.79L12,15.09l8.29-8.3a1,1,0,1,1,1.42,1.42Z" data-name="20 Down Arrow"></path>
-							</svg>
-						</label>
-						<input type='time' placeholder='12:00' />
-						<label>Date
-							<svg viewBox="0 0 24 24">
-								<path d="M21.71,8.21l-9,9a1,1,0,0,1-1.42,0l-9-9A1,1,0,1,1,3.71,6.79L12,15.09l8.29-8.3a1,1,0,1,1,1.42,1.42Z" data-name="20 Down Arrow"></path>
-							</svg>
-						</label>
-						<input type='email' required /> */}
 				</div>
 			</div>
 		</section>
